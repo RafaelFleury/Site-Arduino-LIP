cf2799e00c0afac118668dfa3f3216dc5e3d5e73
Carolina-Nigri <102864941+Carolina-Nigri@users.noreply.github.com>
2022-06-28 16:34:52 -0300

    Update bot

--- Site/PixlrBgResult1/PixlrBgResult1.js
+++ Site/PixlrBgResult1/PixlrBgResult1.js
@@ -3,9 +3,7 @@
 import {
   Sprite,
   Trigger,
-  Costume,
-  Color,
-  Sound
+  Costume
 } from "https://unpkg.com/leopard@^1/dist/index.esm.js";
 
 export default class PixlrBgResult1 extends Sprite {
@@ -20,8 +18,6 @@ export default class PixlrBgResult1 extends Sprite {
       )
     ];
 
-    this.sounds = [];
-
     this.triggers = [
       new Trigger(Trigger.GREEN_FLAG, this.whenGreenFlagClicked),
       new Trigger(Trigger.CLICKED, this.whenthisspriteclicked)
@@ -31,26 +27,28 @@ export default class PixlrBgResult1 extends Sprite {
   *whenGreenFlagClicked() {
     this.goto(30, 0);
     this.size = 30;
-    yield* this.sayAndWait("Clique em mim para obter ajuda", 3);
+    yield* this.say("Clique em mim para obter ajuda");
   }
 
   *whenthisspriteclicked() {
     while (!(this.answer > 0 && this.answer < 4)) {
       yield* this.sayAndWait("Digite 1 para saber mais sobre o Arduino", 3);
-      yield* this.sayAndWait("Digite 2 para ter acesso ao nosso tutorial", 3);
+      yield* this.sayAndWait(
+        "Digite 2 para aprender a programar com Arduino",
+        3
+      );
       yield* this.sayAndWait("Digite 3 para descobrir mais sobre nós", 3);
       yield* this.askAndWait("Qual opção você deseja?");
       yield;
-    }
-    if (this.answer == 1) {
-      window.location.href = "/html/interesting-links.html";
-    } else {
-      if (this.answer == 2) {
+
+      if (this.answer == 1) {
+        window.location.href = "/html/interesting-links.html";
+      } else if (this.answer == 2) {
         window.location.href = "/html/tests.html";
+      } else if (this.answer == 3) {
+        window.location.href = "/html/about.html";
       } else {
-        if (this.answer == 3) {
-          window.location.href = "/html/about.html";
-        }
+        yield* this.sayAndWait("Opção inválida!", 2);
       }
     }
   }
